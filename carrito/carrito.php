8bc82b46912c172c4898a3e04dfc8b7e5779bb45
alex <alejandropelazasbaena@gmail.com>
2024-11-14 18:01:20 +0100

    correccion

--- carrito/carrito.php
+++ carrito/carrito.php
@@ -92,7 +92,7 @@ $nombre = $nombre["nombre"];
   </main>
   <script src="./carrito.js">
   </script>
-  <script script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js">
   </script>
 </body>
 
